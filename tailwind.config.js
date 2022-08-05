019f10c358e7f3fde1f772dd123637c9062e0501
alok1929 <alokhegde221@gmail.com>
2022-08-05 23:07:03 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html"],
+  theme: {
+    colors: {
+      Darkcyan: "hsl(185, 75%, 39%)",
+      Verydarkdesaturatedblue: "hsl(229, 23%, 23%)",
+      Darkgrayishblue: "hsl(227, 10%, 46%)",
+      white: "hsl(0, 0%, 99%)",
+    },
+
+    extend: {},
+  },
+  plugins: [],
+};
